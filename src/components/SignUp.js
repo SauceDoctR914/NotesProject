9a6397efe387c9b53eb22f49fdd1d7113d91ac43
GavIn Sokolof <sokolofg@gmail.com>
2019-04-30 00:00:04 -0400

    random

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -67,7 +67,7 @@ class SignUp extends Component {
   render() {
     return (
       <div className="login-parent">
-        <div className="login-container" />
+        <div className="loginformtitle" />
         <div className="login-div">
           <h1 className="login-title">Sign Up</h1>
           <form
